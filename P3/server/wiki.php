0863b2b2d06c75d70c7adb529af82600b0e1d4e5
sfongx <sleefong@gmail.com>
2018-01-13 12:26:36 -0500

    Changed wiki.php with my own email

--- P3/server/wiki.php
+++ P3/server/wiki.php
@@ -16,7 +16,7 @@ if($radius > 10000){
 	$radius = 10000;
 }
 $curl = curl_init("https://en.wikipedia.org/w/api.php?action=query&prop=coordinates|pageimages|pageterms|info|extracts&colimit=100&piprop=thumbnail&pithumbsize=144&pilimit=50&wbptterms=description&generator=geosearch&ggscoord=${lat}|${long}&ggsradius=${radius}&ggslimit=100&ggsprop=type&format=json&inprop=url");
-curl_setopt($curl, CURLOPT_USERAGENT, 'UMBC_CMSC_433_Project/1.0 (https://www.csee.umbc.edu/~bwilk1/433/; bwilk1@umbc.edu)');
+curl_setopt($curl, CURLOPT_USERAGENT, 'UMBC_CMSC_433_Project/1.0 (https://www.csee.umbc.edu/~bwilk1/433/; sfong2@umbc.edu)');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($curl);
 echo $data;
